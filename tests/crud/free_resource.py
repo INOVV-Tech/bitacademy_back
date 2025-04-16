b9ffa3ee94fcca99c5a31d3f485418e1d09a7f9a
mazc <marcocoimbra01@gmail.com>
2025-04-16 04:43:00 -0300

    Add BitClass & FreeResource abstraction layers

--- tests/crud/free_resource.py
+++ tests/crud/free_resource.py
@@ -3,25 +3,6 @@ import pytest
 from src.shared.infra.repositories.repository import Repository
 
 class Text_FreeResource:
-    ### CRUD
-    
-    def test_create(self):
-        pass
-
-    def test_get_all(self):
-        pass
-
-    def test_get_one(self):
-        pass
-
-    def test_update(self):
-        pass
-    
-    def test_delete(self):
-        pass
-
-    ### LAMBDAS
-
     def test_lambda_create(self):
         pass
 
