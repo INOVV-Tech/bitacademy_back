e082db25035199aeeb7209d575d6d91c28e6d258
mazc <marcocoimbra01@gmail.com>
2025-04-23 14:25:57 -0300

    Add s3 datasource (with localstack tests)

--- src/shared/infra/external/s3_datasource.py
+++ src/shared/infra/external/s3_datasource.py
@@ -1,13 +1,18 @@
 import boto3
+import base64
 
 from src.shared.environments import Environments
 
 class S3Datasource:
     def __init__(self, bucket_name: str, region: str, endpoint_url: str = None):
+        self.region = region
+
         session = boto3.Session(region_name=region)
 
         if Environments.persist_local:
             endpoint_url = 'http://localhost:4566' # localstack
+
+        self.endpoint_url = endpoint_url
 
         self.s3_resource = session.resource('s3', endpoint_url=endpoint_url)
 
@@ -21,3 +26,39 @@ class S3Datasource:
             result.append(f'- {obj.key} ({obj.size} bytes)')
         
         print('\n'.join(result))
+
+    def get_s3_object(self, s3_key: str):
+        return self.s3_resource.Object(self.bucket_name, s3_key)
+
+    def upload_base64_file(self, s3_key: str, base64_data: str, mime_type: str) -> dict:
+        try:
+            response = self.get_s3_object(s3_key).put(
+                Body=base64.b64decode(base64_data), 
+                ContentType=mime_type
+            )
+
+            if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+                return { 'url': self.get_file_url(s3_key) }
+        except:
+            pass
+        
+        return { 'error': f'S3 "upload_base64_file" falhou com a chave "{s3_key}"' }
+
+    def get_file_url(self, s3_key: str) -> str:
+        if self.endpoint_url is not None:
+            return f'{self.endpoint_url}/{self.bucket_name}/{s3_key}'
+
+        return f'https://{self.bucket_name}.s3.{self.region}.amazonaws.com/{s3_key}'
+    
+    def read_base64_file(self, s3_key: str) -> dict:
+        try:
+            s3_obj = self.get_s3_object(s3_key)
+
+            base64_bytes = s3_obj.get()['Body'].read()
+            base64_bytes = base64.b64encode(base64_bytes)
+            
+            return { 'data': base64_bytes.decode('utf8') }
+        except:
+            pass
+
+        return { 'error': f'S3 "read_file" falhou com a chave "{s3_key}"' }
--- tests/s3/test_datasource.py
+++ tests/s3/test_datasource.py
@@ -1,7 +1,7 @@
 import json
 import pytest
 
-from tests.common import load_app_env
+from tests.common import load_app_env, load_resource
 
 load_app_env()
 
@@ -14,14 +14,41 @@ from src.shared.infra.external.s3_datasource import S3Datasource
 class Test_S3Datasource:
     def print_data(self, data: dict) -> None:
         print(json.dumps(data, indent=4, ensure_ascii=False))
-    
-    # @pytest.mark.skip(reason='Done')
-    def test_bucket(self):
-        s3_datasource = S3Datasource(
+
+    def get_datasource(self) -> S3Datasource:
+        return S3Datasource(
             bucket_name=Environments.bucket_name,
             region=Environments.region
         )
+    
+    @pytest.mark.skip(reason='Done')
+    def test_ping_bucket(self):
+        s3_datasource = self.get_datasource()
 
         s3_datasource.debug_bucket()
 
         assert True
+
+    @pytest.mark.skip(reason='Done')
+    def test_upload_base64(self):
+        s3_datasource = self.get_datasource()
+
+        cover_img = load_resource('free_resource_cover_img.jpg', encode_base64=True, base64_prefix='')
+
+        print('cover_img', cover_img[0:200])
+        
+        resp = s3_datasource.upload_base64_file('cover_img.jpg', cover_img, mime_type='image/jpeg')
+
+        self.print_data(resp)
+
+        assert 'error' not in resp
+
+        resp = s3_datasource.read_base64_file('cover_img.jpg')
+
+        assert 'error' not in resp
+
+        resp = s3_datasource.upload_base64_file('cover_img.jpg', resp['data'], mime_type='image/jpeg')
+
+        self.print_data(resp)
+
+        assert 'error' not in resp
