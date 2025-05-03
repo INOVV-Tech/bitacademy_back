9438ee49c750a2085f29a41e85463986b28a7ceb
mazc <marcocoimbra01@gmail.com>
2025-05-02 21:33:10 -0300

    Fix auth dto

--- src/shared/infra/repositories/dtos/auth_authorizer_dto.py
+++ src/shared/infra/repositories/dtos/auth_authorizer_dto.py
@@ -1,6 +1,4 @@
 from src.shared.domain.enums.role import ROLE
-from src.shared.domain.enums.user_status import USER_STATUS
-from src.shared.domain.entities.user import User
 
 class AuthAuthorizerDTO:
     user_id: str
@@ -11,6 +9,18 @@ class AuthAuthorizerDTO:
     email_verified: bool
     phone_verified: bool
 
+    @staticmethod
+    def from_api_gateway(data: dict) -> 'AuthAuthorizerDTO':
+        return AuthAuthorizerDTO(
+            user_id=data['sub'],
+            name=data['name'],
+            email=data['email'],
+            phone=data['phone_number'] if 'phone_number' in data else '',
+            role=ROLE[data['custom:role']] if 'custom:role' in data else ROLE.GUEST,
+            email_verified=bool(data['email_verified']) if 'email_verified' in data else False,
+            phone_verified=bool(data['phone_number_verified']) if 'phone_number_verified' in data else False
+        )
+
     def __init__(self, user_id: str, name: str, email: str, phone: str, role: ROLE, \
         email_verified: bool, phone_verified: bool):
         self.user_id = user_id
@@ -20,15 +30,3 @@ class AuthAuthorizerDTO:
         self.role = role
         self.email_verified = email_verified
         self.phone_verified = phone_verified
-
-    @staticmethod
-    def from_api_gateway(data: dict) -> 'AuthAuthorizerDTO':
-        return AuthAuthorizerDTO(
-            user_id=data['sub'],
-            name=data['name'],
-            email=data['email'],
-            phone=data['phone_number'],
-            role=ROLE[data['custom:role']] if 'custom:role' in data else ROLE.GUEST,
-            email_verified=data['email_verified'],
-            phone_verified=data['phone_verified']
-        )
